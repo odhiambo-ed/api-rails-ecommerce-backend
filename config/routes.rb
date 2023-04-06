23f7981b02c4a14eb7cb81fe2acb70979597e2ef
white3d <odhiambo.edward111@gmail.com>
2023-04-06 21:36:34 +0300

    Edit Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,29 @@
 Rails.application.routes.draw do
   devise_for :customers
   # resources :categories
-  resources :sizes
-  resources :images
-  resources :colors
-  resources :products
-  resources :customers
+  # resources :sizes
+  # resources :images
+  # resources :colors
+  # resources :products
+  # resources :customers
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
-  resources :categories do
-    resources :products, only: [:create, :destroy]
+  resources :categories, only: [:index, :show] do
+    resources :products, only: [:index, :show]
   end
+  
+  resources :products, only: [] do
+    resources :colors, only: [:index, :show]
+    resources :images, only: [:index, :show]
+    resources :sizes, only: [:index, :show]
+  end
+  
+  resources :customers, only: [:index, :show] do
+    resources :products, only: [:index, :show], controller: 'customers/products'
+  end
+  
+  resources :customers_products, only: [:index, :show]
 end
