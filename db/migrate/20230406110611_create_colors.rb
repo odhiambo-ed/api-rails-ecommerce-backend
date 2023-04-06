32fefc862641384f45919373ad1adfb5ad1154ef
white3d <odhiambo.edward111@gmail.com>
2023-04-06 14:08:27 +0300

    Add Color Route

---
+++ db/migrate/20230406110611_create_colors.rb
@@ -0,0 +1,10 @@
+class CreateColors < ActiveRecord::Migration[7.0]
+  def change
+    create_table :colors do |t|
+      t.string :name
+      t.references :product, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
