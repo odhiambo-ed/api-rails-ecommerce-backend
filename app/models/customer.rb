ed20fb2317edd3ea3b970cff501ed67a0fa358d9
white3d <odhiambo.edward111@gmail.com>
2023-04-11 09:49:42 +0300

    Add Token Generate To The Model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,9 +3,18 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+    # has_secure_password
+
     has_and_belongs_to_many :products
 
     def admin?
         admin
     end
+
+    def generate_jwt
+        JWT.encode({ id: self.id, exp: 60.days.from_now.to_i }, Rails.application.secrets.secret_key_base)
+    end
+
+    # attr_reader :password_digest
 end
